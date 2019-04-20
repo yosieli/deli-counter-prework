a3e75d1150e8b89c27bf9bc615e01773fb84cc1e
feven <feventsegay6@gmail.com>
2019-04-20 03:41:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,8 @@
 def line(katz_deli)
   if katz_deli.length ==0  
   puts "The line is currently empty."
-else katz_deli.each_with_index do |index,name|
+else
+  katz_deli.each_with_index do |index,name|
   result= "the line is currently:#{index.to_i+1}.#{name}"
 end
 puts #{result}
