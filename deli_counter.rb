84cd5ce13bf8ddd92ef59567c02dd1936458341e
feven <feventsegay6@gmail.com>
2019-04-20 03:48:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,8 @@ def line(katz_deli)
    # message = "The line is currently:"
     
   katz_deli.each_with_index do |value, index| 
-    message += " #{index.to_i+1}. #{value}"
+    puts " #{index.to_i+1}. #{value}"
   end 
-  puts "#{message}"
+  #puts "#{message}"
   end 
 end 
