583c284e6c1932cb2aa411fd8e258652b1ece7ce
feven <feventsegay6@gmail.com>
2019-04-20 04:17:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,7 @@ def take_a_number(katz_deli,name)
   end
   def now_serving(katz_deli)
     if katz_deli.length ==0
-     puts  "The line is currently empty."
+     puts  "There is nobody waiting to be served!"
    else 
       puts "Currently serving katz_deli[0] ."
       katz_deli.shift()
