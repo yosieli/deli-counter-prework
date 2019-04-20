73d01e3daf502301ba8db22a49dcda34f953b750
feven <feventsegay6@gmail.com>
2019-04-20 04:16:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,12 +20,12 @@ def take_a_number(katz_deli,name)
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
   end
   end
-  def now_serving(katz_deli,name)
+  def now_serving(katz_deli)
     if katz_deli.length ==0
      puts  "The line is currently empty."
    else 
-      puts "Currently serving #{name} ."
-      katz_deli.shift(name)
+      puts "Currently serving katz_deli[0] ."
+      katz_deli.shift()
     end
 end
   
