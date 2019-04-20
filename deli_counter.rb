6706ee39679435b761a1ebe68dcdc3f0f3df3aa3
feven <feventsegay6@gmail.com>
2019-04-20 04:18:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,7 +24,7 @@ def take_a_number(katz_deli,name)
     if katz_deli.length ==0
      puts  "There is nobody waiting to be served!"
    else 
-      puts "Currently serving #{katz_deli[0]} ."
+      puts "Currently serving #{katz_deli[0]}."
       katz_deli.shift()
     end
 end
