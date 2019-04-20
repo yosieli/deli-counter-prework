12bba727bd5e7c25d7317a447f75524bfe81aefb
feven <feventsegay6@gmail.com>
2019-04-20 04:03:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ end
 def take_a_number(katz_deli,name)
   if katz_deli.length==0
     katz_deli.push(name)
-    puts "Welcome, #{name}. You are number #{katz_deli} in line."
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
   end
   end
   
