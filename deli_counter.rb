bad76c282326ab4b393f46cf8323699a9d2bd7bc
feven <feventsegay6@gmail.com>
2019-04-20 03:47:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 # Write your code here.
-def line(array) 
-  if array.length == 0 
+def line(katz_deli) 
+  if katz_deli.length == 0 
     puts "The line is currently empty."
   else 
-    message = "The line is currently:"
+   # message = "The line is currently:"
     
-  array.each_with_index do |value, index| 
+  katz_deli.each_with_index do |value, index| 
     message += " #{index.to_i+1}. #{value}"
   end 
   puts "#{message}"
